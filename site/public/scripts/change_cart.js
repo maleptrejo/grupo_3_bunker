ee6a3a1e71ce9182c52388a82324433b8a777193
Malena Trejo <malenatrejo@MacBook-Pro-de-Malena.local>
2020-07-28 21:45:01 -0300

    update

--- site/public/scripts/change_cart.js
+++ site/public/scripts/change_cart.js
@@ -1,22 +1,22 @@
-let botones= document.getElementsByClassName("cart-edit")
+let cartEdit= document.getElementsByClassName("cart-edit")
 
-Array.from(botones).forEach(boton=> { 
+Array.from(cartEdit).forEach(cartEd=> { 
   
 
-    boton.addEventListener('click', function(e){ 
+    cartEd.addEventListener('click', function(e){ 
         e.preventDefault()
         // alert(this.parentElement.parentElement.href)
    
-        console.log(boton.value)
+        console.log(cartEd.value)
 
-        fetch(this.parentElement.parentElement.href, {
+        fetch(this.dataset.url, {
             method: 'put', 
             headers:{
             'Content-Type':'application/json'
         },
         body: JSON.stringify({
         
-            qty:boton.value,
+            qty:cartEd.value,
             // price: this.parentElement.parentElement.value
         })
         
